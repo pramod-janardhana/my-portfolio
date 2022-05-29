49ed6ec4ebe1ae14d1d9d543e8662a8b8751b745
mr-pramod-98 <mr.pj2k18@gmail.com>
2022-05-29 14:32:05 +0530

    footer design changes

--- src/components/FooterSocialLinks.js
+++ src/components/FooterSocialLinks.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Flex } from "@chakra-ui/layout";
+import { Flex, Spacer } from "@chakra-ui/layout";
 import githubLogo from "../drawables/footer/github_logo.svg";
 import linkedLogo from "../drawables/footer/linkedin_logo.svg";
 import codeLogo from "../drawables/footer/code_logo.svg";
@@ -8,13 +8,27 @@ import "./FooterSocialLinks.css";
 const FooterSocialLinks = () => {
   return (
     <Flex id="footer-social-links">
-      <a href="https://www.linkedin.com/in/pramod-j-a039aa175/" target="_blank">
-        <img src={linkedLogo} alt="linkedin" />
-      </a>
-      <a href="https://github.com/mr-pramod-98" target="_blank">
+      <a
+        href="https://github.com/mr-pramod-98"
+        target="_blank"
+        rel="noreferrer"
+      >
         <img src={githubLogo} alt="github" />
       </a>
-      <a href="https://www.hackerrank.com/pramod_athreya_1" target="_blank">
+      <Spacer id="logo_spacer" />
+      <a
+        href="https://www.linkedin.com/in/pramod-j-a039aa175/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src={linkedLogo} alt="linkedin" />
+      </a>
+      <Spacer />
+      <a
+        href="https://www.hackerrank.com/pramod_athreya_1"
+        target="_blank"
+        rel="noreferrer"
+      >
         <img src={codeLogo} alt="code" />
       </a>
     </Flex>
--- src/views/Footer.js
+++ src/views/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Flex, Divider } from "@chakra-ui/react";
+import { Flex, Divider, Box } from "@chakra-ui/react";
 import FooterSocialLinks from "../components/FooterSocialLinks";
 import FooterNavBar from "../components/FooterNavBar";
 import FooterSignature from "../components/FooterSignature";
@@ -8,10 +8,13 @@ import "./Footer.css";
 function Footer() {
   return (
     <Flex id="footer">
-      <FooterSocialLinks />
-      <FooterNavBar />
-      <Divider h="2px" />
-      <FooterSignature />
+      <Box id="row1">Lets connect</Box>
+      <Flex id="row2">
+        <FooterSocialLinks />
+        <FooterSignature />
+      </Flex>
+      {/* <FooterNavBar /> */}
+      {/* <Divider h="2px" /> */}
     </Flex>
   );
 }
